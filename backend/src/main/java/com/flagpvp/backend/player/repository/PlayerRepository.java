a8b98d8c0b60b43e9715561e18e72b583f61aeb7
snowi <snowisamountry@gmail.com>
2026-08-28 01:32:22 -0400

    Add methods to find and delete a Player entity by their ID

--- backend/src/main/java/com/flagpvp/backend/player/repository/PlayerRepository.java
+++ backend/src/main/java/com/flagpvp/backend/player/repository/PlayerRepository.java
@@ -1,7 +1,21 @@
 package com.flagpvp.backend.player.repository;
 
 import com.flagpvp.backend.player.domain.entity.Player;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
+/**
+ * Repository for player entity.
+ */
 public interface PlayerRepository extends CrudRepository<Player, String> {
+
+    // Finds a player by their ID.
+    @NullMarked
+    Optional<Player> findById(String id);
+
+    // Deletes a player by their ID;
+    @NullMarked
+    void deleteById(String id);
 }
